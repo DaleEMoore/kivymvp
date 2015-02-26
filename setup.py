6c8c4724c379383465802705912c1b2428cc8a10
Martin Krämer <mkraemer.de@gmail.com>
2015-02-26 19:09:32 +0100

    0.1.11

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="kivymvp",
-    version="0.1.10",
+    version="0.1.11",
     description="ui framework for kivy following the MVP pattern",
     url="https://github.com/mkraemer67/kivymvp",
     author="Martin Krämer",
